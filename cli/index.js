badce9717c3a2a83155d22843f0b0f341c08592b
Josh Starrett <jstarrett@roomstogo.com>
2021-05-17 20:59:27 -0400

    add field count tool

--- cli/index.js
+++ cli/index.js
@@ -8,4 +8,5 @@ program
   .command('tree', 'display dependency tree of content type', { executableFile: 'tree' })
   .command('links', 'display the most links a content type\'s entries has', { executableFile: 'links' })
   .command('duplicates', 'displays entries that have duplicate entry titles and content types', { executableFile: 'duplicates' })
+  .command('fields', 'display information about how ofter fields are used in a schema', { executableFile: 'fields' })
   .parse()
